9ef12829bb56574ce30ea62d36821d241ccc41b6
Muhammad Azaan Zafar <61707028+Azaan1610@users.noreply.github.com>
2022-04-08 12:23:27 +0100

    matrix checks uploaded

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -5,7 +5,9 @@ import com.example.grpc.server.grpcserver.PongResponse;
 import com.example.grpc.server.grpcserver.PingPongServiceGrpc;
 import com.example.grpc.server.grpcserver.MatrixRequest;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 import com.example.grpc.server.grpcserver.MatrixReply;
@@ -51,10 +53,94 @@ public class GRPCClientService {
                 try { file.transferTo(fileDestination); }
                 catch (Exception e) { return new uploadController(nameOfFile, fileContentType, "add a file please " + e.getMessage()); }
 			
-		return new uploadController(nameOfFile, fileContentType, "matrix file has been uploaded :)");
+		
+				// this is where the functionality goes for reading in the file and then converting it into a 2d int[][] matrix
+		BufferedReader reader;
+		reader = new BufferedReader(new FileReader(fileDestination));
+		//read in the file from the destination above
+
+		String firstDimension = reader.readLine();
+		String[] split = firstDimension.split(" ");
+		int firstX = Integer.parseInt(split[0]); // get the row
+		int firstY = Integer.parseInt(split[0]);// get the column
+		int[][] first = new int[firstX][firstY]; // make a new 2d int array called first 
+
+		for (int i = 0; i < firstX; i++) { //here we iterate through the FIRST matrix in the file and convert it to a 2d int[][] matrix array
+			String[] line = reader.readLine().split(" ");
+			for (int j = 0; j < firstY; j++) {
+				first[i][j] = Integer.parseInt(line[j]);
+				}
+		}
+
+		// Read "@" from the matrix file
+		reader.readLine(); //this is in place to differentiate the first matrix from the second and vice versa
+		String secondDimension = reader.readLine();
+		String[] split2 = secondDimension.split(" ");
+		int secX = Integer.parseInt(split2[0]);
+		int secY = Integer.parseInt(split2[0]);
+		int[][] second = new int[secX][secY];
+
+		for (int i = 0; i < secX; i++) {
+			String[] line = reader.readLine().split(" ");
+			for (int j = 0; j < secX; j++) {
+				second[i][j] = Integer.parseInt(line[j]);
+				}
+			}
+
+		int[][] matrixA = first; //create a new 2d int array caled matrixA and store the 'first' matrix in it from the code above
+		int[][] matrixB = second;
+		
+		//check size of both matrices to see if they are square
+		String checkSize = sizeCheck(matrixA, matrixB);
+		if (checkSize != "Equal Size :)"){
+			return new uploadController(nameOfFile, fileContentType, checkSize);
+		}
+
+		//given we know that the matrix is a square matrix from the checks above, lets verify if they are square
+		String checkPower = powerCheck(matrixA, matrixB);
+		if (checkPower != "power of 2 :)"){
+			return new uploadController(nameOfFile, fileContentType, checkPower);
+		}
+		return new uploadController(nameOfFile, fileContentType, "File Successfully Uploaded");
 		
 
 
+
+	}
+
+	//class for validating the size of the matrices (both should be sqaure)
+	public static String sizeCheck(int[][]matrixA, int[][]matrixB){
+		int matrixA_row1 = matrixA.length; // checks the length of the row for matrix a (eg how many elements in the row)
+		int matrixA_col1 = matrixA[0].length; // checks the first column [0] of matrixA (eg how many elements in that col)
+		String reply;
+		int matrixB_row1 = matrixB.length;
+		int matrixB_col1 = matrixB[0].length;
+
+		//here we compare to see if both matrices have same length of row and column by splitting eg matrixB[0] gets us the column in the first row
+		if(matrixA_row1!=matrixB_row1 || matrixA_col1!=matrixB_col1){
+				reply = "Matrices are not square!";
+		}
+		else{
+				reply = "Equal Size :)";
+		}
+		return reply;
+	}
+
+	public static String powerCheck(int[][]matrixA, int[][]matrixB){
+		int matrixA_row1 = matrixA.length;
+		int matrixA_col1 = matrixA[0].length;
+		String reply;
+		int matrixB_row1 = matrixB.length;
+		int matrixB_col1 = matrixB[0].length;
+
+		if (matrixA_row1 % 4 !=0 || matrixB_row1 % 4 !=0){ //if matrix length % 4 == 0 it means matrix is power of 2
+			reply = "Not power of 2";
+		}
+		else{
+			reply = "power of 2 :)";
+		}
+
+		return reply;
 
 	}
 
